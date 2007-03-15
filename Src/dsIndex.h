7feeeba7203de8103bad062a064e2aea8e5208eb
sakarab <sakarab@53496c32-c328-0410-8999-9f05d0c63dc9>
2007-03-15 00:43:00 +0000

    Added copyrihgt notice
    git-svn-id: https://svn.code.sf.net/p/smds/code/trunk@10 53496c32-c328-0410-8999-9f05d0c63dc9

--- Src/dsIndex.h
+++ Src/dsIndex.h
@@ -1,3 +1,23 @@
+/****************************************************************************
+  smds - the openSource Memory DataSet
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Please read the "readme" file in the root directory of this
+  package for more copyright and license information.
+****************************************************************************/
 //---------------------------------------------------------------------------
 #ifndef SM_DS_INDEX_H
 #define SM_DS_INDEX_H
--- Test/uTest.cpp
+++ Test/uTest.cpp
@@ -1,3 +1,23 @@
+/****************************************************************************
+  smds - the openSource Memory DataSet
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Please read the "readme" file in the root directory of this
+  package for more copyright and license information.
+****************************************************************************/
 //---------------------------------------------------------------------------
 #pragma hdrstop
 
@@ -71,14 +91,13 @@ tblFiles_ptr GetTblFiles()
     ds::cDbEngine           transport = ds::SelectDbEngine( "BDE" );
     ds::cDatabase           connection = transport.NewConnection( BDE_DirData_Conn );
 */
-/*
+
     ds::cDbEngine           transport = ds::SelectDbEngine( "ADO" );
     ds::cDatabase           connection = transport.NewConnection( ADO_Dirdata_Conn );
-*/
-
+/*
     ds::cDbEngine           transport = ds::SelectDbEngine( "DAO" );
     ds::cDatabase           connection = transport.NewConnection( DAO_Dirdata_Conn );
-
+*/
     tblFiles_ptr            result( new tblFiles() );
 
     result->Open( connection, 0 );
